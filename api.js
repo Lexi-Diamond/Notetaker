596c1615a2899a79bc64bc509f7864ef463f82ed
Lexi Dorrity <lexidorrity@Lexis-MBP.socal.rr.com>
2022-03-31 07:03:37 -0700

    worked on api.js

--- api.js
+++ api.js
@@ -1,24 +1,22 @@
-const { append } = require("express/lib/response");
-const fs = require("fs")
+const uuid = require("uuid");
+const fs = require("fs");
 const router = require('express').Router();
 
-app.get("/api/notes", function(req,res) {
-    readFileAsync("./db/dbjson", "utf8").then(function(data){
-        notes = [].concat(JSON.parse(data))
-        res.json(notes);
-    })
-});
+router.get('/notes', (req,res) => {
+    const data = fs.readFileSync("./db/db.json");
+    jes.json(JSON.parse(data));
+})
 
-app.post("/api/notes", function(req, res) {
-    const note = req.body;
-    readFileAsync("./db/db.json", "utf8").then(function(data) {
-        const notes = [].concat(JSON.parse(data));
-        note.id = notes.length + 1
-        notes.push(note);
-        return notes})
-        .then(function(notes) {
-            writeFileAsync("./db/db.json", JSON.stringify(notes))
-            res.join(note);
-        })
-    });
+router.post("/notes", (req, res) => {
+    const note = JSON.parse(fs.readFileSync("./db/db.json"));
+    const newNote = req.body;
+    newNote.id = uuid.v4();
+    note.push(newNote);
+    fs.writeFileSync(".db.db.json", JSON.stringify(note));
+    res.json(note);
+
+})
+
+
+
 
--- html.js
+++ html.js
@@ -1,12 +1,13 @@
-const { append } = require('express/lib/response');
 const path = require('path');
 const router = require('express').Router();
 
-app.get("/notes", function(req,res) {
+router.get("/notes", (req,res) => {
     res.sendFile(path.join(__dirname, "./develop/public/notes.html"));
 });
 
-app.get('/', function(req, res) {
+router.get('/', (req, res) => {
     res.sendFile(path.join(dirname, "./develop/public/index.html"));
 });
+
+module.exports = router;
 
