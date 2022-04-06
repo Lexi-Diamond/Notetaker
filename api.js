d869699518e4d0ff1ba49152ff6827b7ae43619a
Lexi Dorrity <lexidorrity@Lexis-MBP.socal.rr.com>
2022-04-06 09:15:20 -0700

    fix router module

--- api.js
+++ api.js
@@ -1,13 +1,13 @@
 const uuid = require("uuid");
 const fs = require("fs");
-const app = require('express').Router();
+const router = require('express').Router();
 
-app.get('/notes', (req,res) => {
+router.get('/notes', (req,res) => {
     const data = fs.readFileSync("./db/db.json");
     jes.json(JSON.parse(data));
 })
 
-app.post("/notes", (req, res) => {
+router.post("/notes", (req, res) => {
     const note = JSON.parse(fs.readFileSync("./db/db.json"));
     const newNote = req.body;
     newNote.id = uuid.v4();
@@ -17,7 +17,7 @@ app.post("/notes", (req, res) => {
 
 })
 
-app.delete('/notes/:id', (req, res) => {
+router.delete('/notes/:id', (req, res) => {
     const note = JSON.parse(ds.readFileSync('./db.db.json'));
     const deleteNote = note.filter((rmvNote) => rmvNote.id !== req.params.id);
     fs.writeFileSync('./db.db.json', JSON.stringify(deleteNote));
