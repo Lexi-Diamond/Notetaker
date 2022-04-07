6c9b326912278e1040016ae49aed6cab98d81c4d
Lexi Dorrity <lexidorrity@Lexis-MBP.socal.rr.com>
2022-04-07 06:16:11 -0700

    debug api post route

--- api.js
+++ api.js
@@ -9,12 +9,12 @@ router.get('/notes', (req,res) => {
 
 router.post("/notes", (req, res) => {
     // const note = JSON.parse(fs.readFileSync("./db/db.json"));
-    // const newNote = req.body;
-    console.log(req.body);
-    // newNote.id = uuid();
-    // note.push(newNote);
-    // fs.writeFileSync("./db.db.json", JSON.stringify(note));
-    // res.json(note);
+    const newNote = req.body;
+    newNote.id = uuid.v4();
+    console.log(newNote);
+    note.push(newNote);
+    fs.writeFileSync("./db.db.json", JSON.stringify(newNote));
+    res.json(newNote);
 
 })
 
